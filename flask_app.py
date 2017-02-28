7d1a7cd6ed5d1cf97b59b9a7ffb51e604b16f806
Alex <lispython@users.noreply.github.com>
2017-02-28 11:49:27 +0000

    Update.

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from flask import request, Response, Flask
 
 from string import ascii_letters
 from random import choice
-from pyprometheus.contrib.uwsgi_features import UWSGIStorage
+from pyprometheus.contrib.uwsgi_features import UWSGIStorage, UWSGICollector
 from pyprometheus.registry import BaseRegistry
 from pyprometheus.utils.exposition import registry_to_text
 from pyprometheus.metrics import BaseMetric, Gauge, Counter, Histogram, Summary
@@ -15,10 +15,14 @@ storage = UWSGIStorage(0)
 registry = BaseRegistry(storage=storage)
 
 
-requests_total = Counter("app:requests_total", "Total count of requests", ["method", "url_rule"], registry=registry)
-request_time_histogram = Histogram("app:request_time_histogram", "Request time", ["method", "url_rule"], registry=registry)
-request_time_summary = Histogram("app:request_time_summary", "Request time", ["method", "url_rule"], registry=registry)
+requests_total = Counter("app:requests_total", "Total count of requests", ["method", "url_rule","rnd"], registry=registry)
+request_time_histogram = Histogram("app:request_time_histogram", "Request time", ["method", "url_rule", "rnd"], registry=registry)
+request_time_summary = Histogram("app:request_time_summary", "Request time", ["method", "url_rule", "rnd"], registry=registry)
+requests_in_progress = Gauge("app:requests_in_progress", "Current requests in progress", ["method", "url_rule", "rnd"], registry=registry)
 
+uwsgi_collector = UWSGICollector(namespace="app:head:uwsgi", labels={"env_role": "test"})
+
+registry.register(uwsgi_collector)
 
 
 app = Flask(__name__)
@@ -34,14 +38,16 @@ def metrics():
 @app.route('/<path:path>')
 @app.route('/')
 def index(path='/'):
-    requests_total.labels(method=request.method, url_rule="total").inc()
-
     rnd = ''.join([choice(ascii_letters) for x in xrange(10)])
 
-    text = "# Process in {0} rnd={1}\n".format(os.getpid(), rnd)
-    #with request_time_histogram.labels(method=request.method, url_rule=path).time(), request_time_summary.labels(method=request.method, url_rule=path).time():
+    with requests_in_progress.labels(method=request.method, url_rule="total", rnd=rnd).track_in_progress():
+        requests_total.labels(method=request.method, url_rule="total", rnd=rnd).inc()
 
-    return Response(text, mimetype="text/plain")
+
+
+
+        text = "# Process in {0} rnd={1}\n".format(os.getpid(), rnd)
+        with request_time_histogram.labels(method=request.method, url_rule=path, rnd=rnd).time(), request_time_summary.labels(method=request.method, url_rule=path, rnd=rnd).time():
+            return Response(text, mimetype="text/plain")
 
 application = app
-print("Debug app init")
